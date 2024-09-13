5679ff01a172b42db096e270e6c56ee0f56ec305
grigory-zhovtun <grigory.zhovtun@gmail.com>
2024-09-14 00:34:18 +0500

    Resolve merge conflicts

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,4 +1,5 @@
 type AddressType = {
+<<<<<<< HEAD
   street: any; // ПОДПРАВЛЯЕМ any
   city: any; // ПОДПРАВЛЯЕМ any
 };
@@ -12,10 +13,29 @@ type UserListPropsType = {
 };
 
 export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
+=======
+  street: string;
+  city: string;
+};
+
+type UserType = {
+  id: number,
+  name: string,
+  age: number,
+  address: AddressType,
+};
+
+type UserListPropsType = {
+  users: UserType[];
+};
+
+export const UserList = (props: UserListPropsType) => {
+>>>>>>> 938b289 (fix changes)
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
 
+<<<<<<< HEAD
       {/*<ul>*/}
       {/*  {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...*/}
       {/*    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>*/}
@@ -24,6 +44,16 @@ export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
       {/*    </li>*/}
       {/*  ))}*/}
       {/*</ul>*/}
+=======
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id} id={`hw01-user-${user.id}`}>
+            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+            {user.address.street}, {user.address.city}
+          </li>
+        ))}
+      </ul>
+>>>>>>> 938b289 (fix changes)
     </div>
   );
 };
